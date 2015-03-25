62c0890d37e743a026299d1dc96621b61df3536d
Justin Tai <taijusti@gmail.com>
2015-03-25 13:13:57 -0400

    n != 0 assertion error. believe the problem is the input data cannot have duplicate points. maybe should add support for n <= 0

--- sw/smo/smo.c
+++ sw/smo/smo.c
@@ -13,6 +13,7 @@
 #define bool int
 #define TEST_DIM (2)
 #define TEST_DATA_SIZE (100)
+#define KKT_THRESHOLD (0.001)
 
 typedef struct {
 	int * dim;
@@ -20,7 +21,7 @@ typedef struct {
 } DATA;
 
 typedef struct {
-	double * dim; // dimensions of weight vector
+	double * alpha; // Lagrange multipliers
 	double b; // bias
 } CLASSIFIER;
 
@@ -30,74 +31,54 @@ int dotProduct(DATA * a, DATA * b, int dim){
 
 	for (; i < dim; i++) {
 		sum += a->dim[i] * b->dim[i];
-		//printf("qedewde data 1 %i   data 2  %i\n", a->dim[i], b->dim[i]);
 	}
 
 	return sum;
 }
+// linear kernel
+int kernel(DATA * a, DATA * b, int numDim) {
+	return dotProduct(a, b, numDim);
+}
 
-int classify(DATA * x, CLASSIFIER * classifier, int numDim) {
+double classify(DATA * x, DATA * trainData, CLASSIFIER * classifier, int numDim, int numData) {
 	int i;
-	int sum = 0;
+	double sum = 0;
 
-	for (i = 0; i < numDim; i++) {
-		sum += x->dim[i] * classifier->dim[i];
+	for (i = 0; i < numData; i++) {
+		sum += trainData[i].y * classifier->alpha[i] * kernel(x, trainData + i, numDim);
 	}
 	sum -= classifier->b;
 
 	return sum;
 }
 
-double classify_2(DATA * x, double * alpha, double b, int numData, int numDim, int classified_point)
-{
-	int i;
-	double sum = 0;
-	for (i = 0; i < numData; i++)
-	{
-		sum += alpha[i] * x[i].y * kernel(&x[i], &x[classified_point], numDim);
+// function to check if a point satisfies KKT conditions
+bool checkKKT(int idx, DATA * trainData, CLASSIFIER * classifier, int numDim, int numData) {
+	double yuProduct = trainData->y
+        * classify(trainData + idx, trainData, classifier, numDim, numData);
+
+	if (0 == classifier->alpha[idx]) {
+		return yuProduct >= (1 - KKT_THRESHOLD);
 	}
-
-	sum -= b;
-	return sum;
-}
-
-// linear transform
-DATA * transform(DATA * a) {
-	return a;
-}
-
-int kernel(DATA * a, DATA * b, int numDim) {
-	return dotProduct(transform(a), transform(b), numDim);
-}
-
-bool checkKKT(double alpha, double yuProduct) {
-
-
-	if (0 == alpha) {
-		return yuProduct >= 1 - 0.001;
-	}
-	else if (C == alpha) {
-		return yuProduct <= 1 + 0.001;
+	else if (C == classifier->alpha[idx]) {
+		return yuProduct <= (1 + KKT_THRESHOLD);
 	}
 	else {
 		// if we're here, assume that 0 < alpha < C
-		return ((yuProduct < 1.001) && (yuProduct > 0.999));
+		return yuProduct < (1 + KKT_THRESHOLD) && yuProduct > (1 - KKT_THRESHOLD);
 	}
 }
 
 void smo(DATA * data, CLASSIFIER * classifier, int numDim, int numData) {
-	double * alpha = (double*)malloc(sizeof(double) * numData); // Langrange multipliers
-	memset(alpha, 0, sizeof(double) * numData);
-	double * err = (double*)malloc(sizeof(double) * numData); // error cache, TODO: cache the non-bound errors only
-	memset(err, 0, sizeof(double) * numData);
-
-	// TODO zero out classifier 
-	int n, start;
+	//double * err = (double*)malloc(sizeof(double) * numData); // error cache
+	//memset(err, 0, sizeof(double) * numData);
+    double * alpha = classifier->alpha; // alias alpha for the sake of berevity in the code
+    memset(alpha, 0, sizeof(double) * numData);
+    classifier->b = 0;
+	int n;
 	int i, j;
-	double yuProduct;
 	double bNew, bOld = 0;
 	double b1, b2, k11, k12, k22;
-	bool done = true;
 	int d1Idx;
 	int d2Idx;
 	double maxErr;
@@ -107,202 +88,201 @@ void smo(DATA * data, CLASSIFIER * classifier, int numDim, int numData) {
 	int s;
 	double low;
 	double high;
-	bool flag = 1;
-	start = -1;
-	for (i = 0; i<numData; i++)
-	{
-		err[i] = -data[i].y; // initialiye error cache with error values, since all initial alphas and threshold are zero then all classifications are zero. (I think)
-	}
+    int iteration = 0;
+    double err1 = 0;
+    double err2 = 0;
+    double err = 0;
+    int start = 0;
+    int idx = 0;
+    bool flag = false;
 
-
-
+    // initial error computation
+    //for (i = 0; i < numData; i++) {
+    //    err[i] = -data[i].y;
+    //}
 
 	while (1) {
 		// Heuristic to choose first datum to work on. Find the first point
-		// that violates KK
-		start = (start + 1); // variable to choose different kkt violator each iteration
-		//printf ("start %i \n", start);	
-		if (start == 100000)
-			break;
-		for (i = start; i < numData + start; i++) {
-			yuProduct = data[i%numData].y * classify_2(data, alpha, bOld, numData, numDim, i%numData);
-
-			if (checkKKT(alpha[i%numData], yuProduct) == false) {
-				d2Idx = i%numData;
-				flag = 0;
+		// that violates KKT
+		flag = false;
+        start = rand() % numData;
+		for (i = 0; i < numData; i++) {
+            idx = (i + start) % numData;
+			if (checkKKT(idx, data, classifier, numDim, numData) == false) {
+				d2Idx = idx;
+                err2 = classify(data + idx, data, classifier, numDim, numData) - data[idx].y;
+                flag = true;
 				break;
 			}
+		}
 
-		}
-	
-		if (flag) {
-			break;
-		}
+        if (flag == false) {
+            return;
+        }
 
 		// Heuristic to choose second element. Just choose any element that maximizes error
-		//d2Idx = 0;
 		maxErr = 0;
 		d1Idx = 0;
 		for (i = 0; i < numData; i++) {
-			if (((ABS(err[d2Idx] - err[i]) > maxErr))) {
-				maxErr = ABS(err[d2Idx] - err[i]);
+            err = classify(data + i, data, classifier, numDim, numData) - data[i].y;
+			//if (((ABS(err[d2Idx] - err[i]) > maxErr))) {
+			if (ABS(err - err2) > maxErr) {
+                err1 = err;
+				maxErr = ABS(err1 - err2);
 				d1Idx = i;
 			}
 		}
 
-		// STEP 1: compute N
-		//	printf ("i %i j %i \n", d1Idx,d2Idx);
-		//	printf ("error %f \n", maxErr);		
+        printf("i: %d, point1: %d, point2: %d\n", iteration, d1Idx, d2Idx);
+        iteration++;
 
+		// STEP 1: compute N
 		k11 = kernel(&(data[d1Idx]), &(data[d1Idx]), numDim);
 		k22 = kernel(&(data[d2Idx]), &(data[d2Idx]), numDim);
 		k12 = kernel(&(data[d1Idx]), &(data[d2Idx]), numDim);
 		n = k11 + k22 - (2 * k12);
 
+		// STEP 2: compute unclipped alpha2New
+		assert (n != 0);//TODO: check what to do if n is zero.
+		alpha2New = alpha[d2Idx] + (((data[d2Idx].y * (err1 - err2)) / (n)));//TODO: check what to do if n is zero.
 
-		// STEP 2: compute error
-		//err[d1Idx] = classify(&(data[d1Idx]), classifier, numDim) - data[d1Idx].y;
-		//err[d2Idx] = classify(&(data[d2Idx]), classifier, numDim) - data[d2Idx].y;
-		//err[d1Idx] = classify_2(data, alpha, bOld, numData, numDim, d1Idx) - data[d1Idx].y; //get them free from cache. TODO: if nonboundthen compute it
-		//err[d2Idx] = classify_2(data, alpha, bOld, numData, numDim, d2Idx) - data[d2Idx].y; //we get them from the cache
-
-
-
-
-		// STEP 3: compute unclipped alpha2New
-		
-		
-		assert (n!=0);//TODO: check what to do if n is zero.
-		alpha2New = alpha[d2Idx] + (((data[d2Idx].y * (err[d1Idx] - err[d2Idx])) / (n)));//TODO: check what to do if n is zero.
-
-		if (data[d1Idx].y == data[d2Idx].y) 
-		{
+		if (data[d1Idx].y == data[d2Idx].y) {
 			low = MAX(0, alpha[d2Idx] + alpha[d1Idx] - C);
 			high = MIN(C, alpha[d1Idx] + alpha[d2Idx]);
 		}
-		else 
-		{
+		else {
 			low = MAX(0, alpha[d2Idx] - alpha[d1Idx]);
 			high = MIN(C, C + alpha[d2Idx] - alpha[d1Idx]);
 		}
-		if (low == high)
-		{
-			
-			continue;
-		}
 
-
-		// STEP 5: clip alpha2
+		// STEP 3: clip alpha2
 		alpha2NewClipped = MIN(alpha2New, high);
 		alpha2NewClipped = MAX(alpha2NewClipped, low);
 
-		// STEP 6: compute alpha1
+		// STEP 4: compute alpha1
 		s = data[d1Idx].y * data[d2Idx].y;
 		alpha1New = alpha[d1Idx] + s * (alpha[d2Idx] - alpha2NewClipped);
 				
-		// STEP 7: compute w. non-linear svm so no w
-		//w += alpha1New *;
-
-		// STEP 8: update values
-		// TODO: update error cache, since it is used to decide the next alphas
-		// updating the threshold		
-		b1 = err[d1Idx] + (data[d1Idx].y * (alpha1New - alpha[d1Idx]) * k11) + (data[d2Idx].y * (alpha2NewClipped - alpha[d2Idx]) * k12) + bOld;
-		b2 = err[d2Idx] + (data[d1Idx].y * (alpha1New - alpha[d1Idx]) * k12) + (data[d2Idx].y * (alpha2NewClipped - alpha[d2Idx]) * k22) + bOld;
+		// STEP 5: update bias value
+		b1 = err1
+             + (data[d1Idx].y * (alpha1New - alpha[d1Idx]) * k11)
+             + (data[d2Idx].y * (alpha2NewClipped - alpha[d2Idx]) * k12)
+             + bOld;
+		b2 = err2
+             + (data[d1Idx].y * (alpha1New - alpha[d1Idx]) * k12)
+             + (data[d2Idx].y * (alpha2NewClipped - alpha[d2Idx]) * k22)
+             + bOld;
 		if ((alpha1New > 0) && (alpha1New < C))
-			bNew = b1;
+			classifier->b = b1;
 		else if ((alpha2NewClipped > 0) && (alpha2NewClipped < C))
-			bNew = b2;
+			classifier->b = b2;
 		else
-			bNew = (b1 + b2) / (2);
-		// updating the error cache
+			classifier->b = (b1 + b2) / (2);
 
-		for (i = 0; i<numData; i++) // TODO: update only non-bound alphas and if d1Idx or d2Idx are in bound then their correponding error would be zero
-			{
+		// STEP 6: update error cache
+		/*
+		err[d1Idx] = 0;
+        err[d2Idx] = 0;
+		for (i = 0; i < numData; i++) {
+            // we don't care about updating the error cache if it is a
+            // bound point or if the point is the one of the two we just updated
+            if (alpha[i] == C || alpha[i] == 0 || i == d1Idx || i == d2Idx) {
+                continue;
+            }
 
-				err[i] = err[i] + data[d1Idx].y * (alpha1New - alpha[d1Idx])*kernel(&(data[d1Idx]), &(data[i]), numDim) + data[d2Idx].y * (alpha2NewClipped - alpha[d2Idx]) * kernel(&(data[d2Idx]), &(data[i]), numDim) + bOld - bNew;
-			}
-				//printf("bold %f, bnew %f ",bOld,bNew);
-			bOld = bNew;
-			alpha[d1Idx] = alpha1New;
-			alpha[d2Idx] = alpha2NewClipped;
-				//if (alpha1New > 0 | alpha2NewClipped > 0)
-			//	printf("alpha: %i, %f alpha: %i, %f. \n", d1Idx, alpha1New, d2Idx, alpha2NewClipped);
+		    err[i] = err[i]
+                     + data[d1Idx].y * (alpha1New - alpha[d1Idx]) * kernel(&(data[d1Idx]), &(data[i]), numDim)
+                     + data[d2Idx].y * (alpha2NewClipped - alpha[d2Idx]) * kernel(&(data[d2Idx]), &(data[i]), numDim)
+                     + bOld - classifier->b;
+		}
+        */
 
+		bOld = classifier->b;
+		alpha[d1Idx] = alpha1New;
+		alpha[d2Idx] = alpha2NewClipped;
 	}
-	/////////////////////////////// computing w only for linear to use the test in the main temporary!!!!!!!
-	for (i = 0; i<numData; i++)
-	{
-		for (j = 0; j<numDim; j++)
-			classifier->dim[j] += alpha[i] * data[i].dim[j];
-	}
-	//for (j = 0; j<numDim; j++)
-	//printf("w %f, b %f \n", classifier->dim[j], bOld);
+
 	// cleanup
-	free(alpha);
-	free(err);
+	//free(err);
 }
 
 // tests the SMO implementation
 int main(void) {
-	// randomly choose a weight vector and bias
-
-
-
-
-	//printf(" wefwe %06.3f", 3.0);
 	CLASSIFIER theoreticalClassifier;
 	int i;
 	int j;
 
-	theoreticalClassifier.dim = (double*)malloc(sizeof(double) * TEST_DIM);
-	for (i = 0; i < TEST_DIM; i++) {
-
-		theoreticalClassifier.dim[i] = (rand()) % 11 - 6;
-		printf(" %06.3f\n", theoreticalClassifier.dim[i]);
+    // randomly choose a theoretical classifier
+	theoreticalClassifier.alpha = (double*)malloc(sizeof(double) * TEST_DATA_SIZE);
+	for (i = 0; i < TEST_DATA_SIZE; i++) {
+		theoreticalClassifier.alpha[i] = (rand()) % 11 - 6;
 	}
 	theoreticalClassifier.b = (rand() % 10) - 5;
+
+    // print out the theoretical classifier
+    printf("theoreticalClassifier {alpha = ");
+    for (i = 0; i < TEST_DATA_SIZE; i++) {
+        printf("%f ", theoreticalClassifier.alpha[i]);
+    }
+    printf(", b = %f}\n", theoreticalClassifier.b);
 
 	// generate test data based on chosen classifier
 	DATA trainingData[TEST_DATA_SIZE];
 
-	for (i = 0; i < TEST_DATA_SIZE; i++) {
+    for (i = 0; i < TEST_DATA_SIZE; i++) {
 		trainingData[i].dim = (int*)malloc(sizeof(int) * TEST_DIM);
-		for (j = 0; j < TEST_DIM; j++) {
+        for (j = 0; j < TEST_DIM; j++) {
 			trainingData[i].dim[j] = (rand()) % 10 - 5;
-		}
-		
+        }
+    }
 
-		if (classify(&(trainingData[i]), &theoreticalClassifier, TEST_DIM) > 0)
+	for (i = 0; i < TEST_DATA_SIZE; i++) {
+		if (classify(trainingData + i, trainingData, &theoreticalClassifier, TEST_DIM, TEST_DATA_SIZE) > 0)
 			trainingData[i].y = 1;
 		else
 			trainingData[i].y = -1;
-
 	}
 
 	// try training using SMO
 	CLASSIFIER experimentalClassifier;
-	experimentalClassifier.dim = (double*)malloc(sizeof(double) * TEST_DIM);
+	experimentalClassifier.alpha = (double*)malloc(sizeof(double) * TEST_DATA_SIZE);
 	smo(trainingData, &experimentalClassifier, TEST_DIM, TEST_DATA_SIZE);
-	//printf(" %06.3f\n",  (5.0/9.0)*(3535-32));
+
+
+    // print out the experimental classifier
+    printf("experimentalClassifier {w = ");
+    for (i = 0; i < TEST_DIM; i++) {
+        printf("%f ", experimentalClassifier.alpha[i]);
+    }
+    printf(", b = %f}\n", experimentalClassifier.b);
+
 	// try classifying with the experimentalClassifier
 	DATA testData;
+    testData.dim = (int *)malloc(sizeof(int) * TEST_DIM);
 	int expectedResult;
 	int actualResult;
 	int mispredicts = 0;
 	for (i = 0; i < TEST_DATA_SIZE; i++) {
 		for (j = 0; j < TEST_DIM; j++) {
-			testData.dim[j] = (rand()) - 100;
+			testData.dim[j] = rand() - 100;
 		}
 
-		expectedResult = classify(&testData, &theoreticalClassifier, TEST_DIM);
-		actualResult = classify(&testData, &experimentalClassifier, TEST_DIM);
+		expectedResult = classify(&testData, trainingData, &theoreticalClassifier, TEST_DIM, TEST_DATA_SIZE);
+		actualResult = classify(&testData, trainingData, &experimentalClassifier, TEST_DIM, TEST_DATA_SIZE);
 
 		if (expectedResult != actualResult) {
+            printf("mispredict! idx: %d expected: %d actual: %d\n", i, expectedResult, actualResult);
 			mispredicts++;
 		}
 	}
 
 	printf("mispredicts: %d\n", mispredicts);
+
+    // cleanup
+    free(testData.dim);
+    for (i = 0; i < TEST_DATA_SIZE; i++) {
+        free(trainingData[i].dim);
+    }
 
 	return 0;
 }

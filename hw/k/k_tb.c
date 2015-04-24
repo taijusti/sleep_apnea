a47ae6acc324da45772fb2a34b83e47a366588e0
ibrahimahmed <Ibrahim.ahmed73@gmail.com>
2015-04-24 16:46:22 -0400

    Update k_tb.c

--- hw/k/k_tb.c
+++ hw/k/k_tb.c
@@ -1,44 +1,79 @@
-#include "k_engine.h"
+#include "../k/k_inc.h"
+#include "../common/common.h"
 #include <stdio.h>
 
+float randFloat(void) {
+	return (float)rand() / 100.0;
+}
+
+ float sw_k(data_t * point1, data_t * point2) {
+	 int i;
+	 float difference;
+	 float result = 0;
+	 for (i=0;i<DIMENSIONS;i++)
+	 {
+		 difference = point1->dim[i]-point2->dim[i];
+		 result = result + difference * difference;
+
+	 }
+	 result = result*-1*inverse_sigma_squared;
+	 return expf(result);
+}
 
 int main()
 {
 
-int x[4] = {2,5,3,-1};
-int y[4] = {5,7,-4,-1};
+//int i[4] = {2,5,3,-1};
 
-float out_actual;
+		data_t point1;
+		data_t point2;
+		data_t data[ELEMENTS];
+		float k1[ELEMENTS];
+		float k2[ELEMENTS];
+		unsigned short i, j;
+		unsigned int errors = 0;
 
-int i;
-k_engine(x,y,&out_actual);
-int difference;
-int total =0;
-for (i=0;i<dim;i++)
-{
+		// randomly generate point1
+		for (i = 0; i < DIMENSIONS; i++) {
+			point1.dim[i] = randFloat();
+		}
 
-difference = x[i]-y[i];
-total = total + difference*difference;
+		// randomly generate point2
+		for (i = 0; i < DIMENSIONS; i++) {
+			point2.dim[i] = randFloat();
+		}
 
-}
- float xx = -1.0*inverse_sigma_squared*(float)total;
-float out_theor = exp(xx);
-
-if (out_theor!=out_actual)
-{
-	printf("%f %f fail %f\n", out_theor,xx,out_actual);
-	return -1;
-
-}
-else
-{
-	printf("%f success %f\n", out_theor,out_actual);
-	return 0;
-
-}
+		// randomly generate some data
+		for (i = 0; i < ELEMENTS; i++) {
+			for (j = 0; j < DIMENSIONS; j++) {
+				data[i].dim[j] = randFloat();
+			}
+		}
 
 
 
+		// call the module being tested
+		k_engine(&point1, &point2, data, k1, k2);
 
+		// test to make sure every k value lines up with the kernel function
+		for (i = 0; i < ELEMENTS; i++) {
+			if (k1[i] != sw_k(&point1, data + i)) {
+				printf("k1 error. Expected: %f Actual: %f\n", sw_k(&point1, data + i), k1[i]);
+				errors++;
+			}
+
+			if (k2[i] != sw_k(&point2, data + i)) {
+				printf("k2 error. Expected: %f Actual: %f\n", sw_k(&point2, data + i), k2[i]);
+				errors++;
+			}
+		}
+
+		if (errors != 0) {
+			printf("TEST_FAILED!\n");
+		}
+		else {
+			printf("TEST PASSED!\n");
+		}
+		return errors;
 
 }

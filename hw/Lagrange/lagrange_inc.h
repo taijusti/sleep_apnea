ebbe65fadb8a7c5256b52b185a708a8fdf741af8
Patrick Wu <p-wu@users.noreply.github.com>
2015-04-29 10:19:47 -0400

    Change tabs to spaces, not yet verified

--- hw/Lagrange/lagrange_inc.h
+++ hw/Lagrange/lagrange_inc.h
@@ -6,19 +6,19 @@
 #define C (100) // Hyperparameter for Lagrange multiplier. All alphas must obey 0 <= alpha <= C
 
 int lagrange(
-		float alpha2_old,
-		float alpha1_old,
-		bool y2,
-		bool y1,
-		float eta,
-		float e2,
-		float e1,
-		float b,
-		float k11,
-		float k12,
-		float k22,
-		float *alpha2_new,
-		float *alpha1_new
-	);
+        float alpha2_old,
+        float alpha1_old,
+        bool y2,
+        bool y1,
+        float eta,
+        float e2,
+        float e1,
+        float b,
+        float k11,
+        float k12,
+        float k22,
+        float *alpha2_new,
+        float *alpha1_new
+    );
 
 #endif

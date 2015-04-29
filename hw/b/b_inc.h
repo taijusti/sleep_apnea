53f1aa0f2ae2b86e8a4aa7f09a8b7d68570a156d
Patrick Wu <p-wu@users.noreply.github.com>
2015-04-29 10:16:47 -0400

    Change tabs to spaces, not yet verified

--- hw/b/b_inc.h
+++ hw/b/b_inc.h
@@ -6,19 +6,20 @@
 #define C (100) // Hyperparameter for Lagrange multiplier. All alphas must obey 0 <= alpha <= C
 
 void b(
-		float e1,
-		float e2,
-		bool y1,
-		bool y2,
-		float alpha1_new,
-		float alpha2_new,
-		float alpha1_old,
-		float alpha2_old,
-		float k11,
-		float k12,
-		float k22,
-		float b_old,
-		float *b_new
-	);
+        float e1,
+        float e2,
+        bool y1,
+        bool y2,
+        float alpha1_new,
+        float alpha2_new,
+        float alpha1_old,
+        float alpha2_old,
+        float k11,
+        float k12,
+        float k22,
+        float b_old,
+        float *b_new
+    );
 
 #endif
+

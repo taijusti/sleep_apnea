c05646c2a11639f9390be666bfabf3c902597f7e
ibrahimahmed <Ibrahim.ahmed73@gmail.com>
2015-06-21 23:52:39 -0400

    Update e.cpp

--- hw/e/e.cpp
+++ hw/e/e.cpp
@@ -6,26 +6,34 @@
 #include <stdint.h>
 #include <hls_stream.h>
 
-void e(hls::stream<float> & e_bram_in, hls::stream<float> & e_bram_out,
+void e( float e_bram[ELEMENTS],
        hls::stream<float> & e_fifo, hls::stream<float> & k0,
        hls::stream<float> & k1, float y1_delta_alpha1_product,
        float y2_delta_alpha2_product, float delta_b) {
-    //#pragma HLS INLINE
-    //#pragma HLS DATAFLOW
+    /*
+    #pragma HLS INTERFACE axis port=k1
+    #pragma HLS INTERFACE axis port=k0
+    #pragma HLS INTERFACE axis port=e_bram_out
+    #pragma HLS INTERFACE axis port=e_bram_in
+    #pragma HLS INTERFACE axis port=e_fifo
+    #pragma HLS INTERFACE s_axilite port=return bundle=e_bus
+    #pragma HLS INTERFACE s_axilite port=delta_b bundle=e_bus
+    #pragma HLS INTERFACE s_axilite port=y2_delta_alpha2_product bundle=e_bus
+    #pragma HLS INTERFACE s_axilite port=y1_delta_alpha1_product bundle=e_bus
+    */
+   // #pragma HLS INLINE
 
     unsigned short i;
 
     for (i = 0; i < PARTITION_ELEMENTS; i++) {
-    //#pragma HLS PIPELINE
-        float e_bram_read = e_bram_in.read();
-        float k0_read = k0.read();
-        float k1_read = k1.read();
-        float temp = e_bram_read
-        		+ (y1_delta_alpha1_product * k0_read)
-                + (y2_delta_alpha2_product * k1_read)
+    #pragma HLS PIPELINE II=4
+        float temp = e_bram[i]
+        		+ (y1_delta_alpha1_product * k0.read())
+                + (y2_delta_alpha2_product * k1.read())
                 + delta_b;
-        assert(temp < 100 && temp > -100);
-        e_bram_out.write(temp);
+      //  e_bram_out.write(temp);
         e_fifo.write(temp);
+        e_bram[i]=temp;
+
     }
 }

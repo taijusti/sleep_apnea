a15184b16929a4766c0b67fabbbdb61dd9e5b023
ibrahimahmed <Ibrahim.ahmed73@gmail.com>
2015-06-21 23:46:10 -0400

    Update delta_e.cpp

--- hw/delta_e/delta_e.cpp
+++ hw/delta_e/delta_e.cpp
@@ -7,20 +7,24 @@
 using namespace std;
 
 void delta_e(float target_e, float e_bram [ELEMENTS], float & max_delta_e, uint32_t & max_delta_e_idx) {
-	//#pragma HLS DATAFLOW
+    /*
+    #pragma HLS INTERFACE s_axilite port=return bundle=delta_e_bus
+    #pragma HLS INTERFACE s_axilite port=target_e bundle=delta_e_bus
+    #pragma HLS INTERFACE s_axilite port=max_delta_e_idx bundle=delta_e_bus
+    #pragma HLS INTERFACE s_axilite port=max_delta_e bundle=delta_e_bus
+    #pragma HLS INTERFACE s_axilite port=e_bram bundle=delta_e_bus
+    */
+    #pragma HLS INLINE
+
     uint32_t i;
     float delta_e;
 
     max_delta_e = -1;
 
     for (i = 0; i < ELEMENTS; i++) {
-    //#pragma HLS PIPELINE
-    //#pragma HLS UNROLL factor=16
+    #pragma HLS PIPELINE
 
-        delta_e = target_e - e_bram[i]; // TODO: cleanup
-        if (delta_e < 0) {
-        	delta_e = delta_e * -1;
-        }
+        delta_e = ABS(target_e - e_bram[i]);
 
         if (delta_e > max_delta_e) {
             max_delta_e = delta_e;

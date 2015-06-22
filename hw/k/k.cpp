339355e08916ebf24650f92f563051673c5e9cdc
ibrahimahmed <Ibrahim.ahmed73@gmail.com>
2015-06-21 23:54:42 -0400

    Update k.cpp

--- hw/k/k.cpp
+++ hw/k/k.cpp
@@ -3,19 +3,24 @@
 #include "../common/common.h"
 #include <stdint.h>
 
-using namespace std;
+static float two_norm(data_t & point1, data_t & point2) {
+#pragma HLS INLINE
 
-static float two_norm(data_t & point0, data_t & point1) {
-	//#pragma HLS PIPELINE
-	//#pragma HLS INLINE
+
+
+
+///	#pragma HLS PIPELINE
+//	#pragma HLS INLINE
 
     float temp = 0;
 	float difference;
     uint32_t i;
-
+    kengineDim:
     for (i = 0; i < DIMENSIONS; i++) {
-	#pragma HLS UNROLL
-        difference = point0.dim[i] - point1.dim[i];
+#pragma HLS PIPELINE
+
+//	#pragma HLS UNROLL
+        difference = point1.dim[i] - point2.dim[i];
         temp += difference * difference;
     }
 
@@ -23,41 +28,50 @@ static float two_norm(data_t & point0, data_t & point1) {
 }
 
 static float exponential(float & x) {
-	//#pragma HLS PIPELINE
-	//#pragma HLS INLINE
-
-	//float temp = x.to_float(); // TODO: for debug
-	//temp = expf((-temp) * inverse_sigma_squared);
-	float temp = expf(-x);
-	return temp;
+//	#pragma HLS PIPELINE
+//	#pragma HLS INLINE
+	return expf(-x);
 }
 
-static float k_engine_help(data_t & point0, data_t & point1) {
-#pragma HLS INLINE
-	//#pragma HLS PIPELINE
-	//#pragma HLS INLINE
+static float k_engine_help(data_t & point1, data_t & point2) {
+//	#pragma HLS PIPELINE
+	#pragma HLS INLINE
 
-    float temp = two_norm(point0, point1);
+
+    float temp = two_norm(point1, point2);
     return exponential(temp);
 }
 
 // used manager side, should be latency optimized
-float k (data_t & point0, data_t & point1) {
-    return k_engine_help(point0, point1);
+float k (data_t & point1, data_t & point2) {
+    return k_engine_help(point1, point2);
 }
 
 // should be throughput optimized
-void k (data_t & point0, data_t & point1, hls::stream<data_t> & data,
-		hls::stream<float> & k0, hls::stream<float> & k1) {
+void k (data_t & point1, data_t & point2, hls::stream<data_t> & data_fifo,
+		hls::stream<float> & k1, hls::stream<float> & k2) {
+    /*
+    #pragma HLS INTERFACE s_axilite port=return bundle=k_bus
+    #pragma HLS INTERFACE axis port=data
+    #pragma HLS INTERFACE axis port=k2
+    #pragma HLS INTERFACE axis port=k1
+    #pragma HLS INTERFACE s_axilite port=point1 bundle=k_bus
+    #pragma HLS INTERFACE s_axilite port=point2 bundle=k_bus
+    */
 	//#pragma HLS INLINE
-	//#pragma HLS DATAFLOW
 
     int i;
+    float temp1,temp2;
 
+
+    KengineLoop:
     for (i = 0; i < PARTITION_ELEMENTS; i++) {
-		#pragma HLS PIPELINE
-        data_t temp = data.read();
-        k0.write(k_engine_help(point0, temp));
+		#pragma HLS PIPELINE II=4
+
+        data_t temp = data_fifo.read();
+        temp1 = k_engine_help(point1, temp);
+        temp2 = k_engine_help(point2, temp);
         k1.write(k_engine_help(point1, temp));
+        k2.write(k_engine_help(point2, temp));
     }
 }

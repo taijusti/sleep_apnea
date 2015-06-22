8c9af2599fac44704e57ffbb3b83cb5a7fe34791
ibrahimahmed <Ibrahim.ahmed73@gmail.com>
2015-06-21 23:43:02 -0400

    Update common.cpp

--- hw/common/common.cpp
+++ hw/common/common.cpp
@@ -3,13 +3,13 @@
 #include "../common/common.h"
 
 float dotProduct(data_t * point1, data_t * point2) {
-    //#pragma HLS PIPELINE
+    #pragma HLS PIPELINE
 
     uint16_t i;
     float sum = 0;
 
     for (i = 0; i < DIMENSIONS; i++) {
-    //#pragma HLS UNROLL
+    #pragma HLS UNROLL
         sum += point1->dim[i] * point2->dim[i];
     }
 
@@ -17,37 +17,41 @@ float dotProduct(data_t * point1, data_t * point2) {
 }
 
 void send(bool y, hls::stream<transmit_t> &fifo) {
+#pragma HLS INLINE
+
 	transmit_t temp;
 	temp.b = y;
 	fifo.write(temp);
 }
 
 void send(uint32_t ui, hls::stream<transmit_t> &fifo) {
+#pragma HLS INLINE
+
 	transmit_t temp;
 	temp.ui = ui;
 	fifo.write(temp);
 }
 
 void send(int32_t i, hls::stream<transmit_t> &fifo) {
+#pragma HLS INLINE
+
 	transmit_t temp;
 	temp.i = i;
 	fifo.write(temp);
 }
-/*
-void send(fixed_t &f, hls::stream<transmit_t> &fifo) {
-	transmit_t temp;
 
-	temp.ui = f.range(31, 0);
-	fifo.write(temp);
-}
-*/
 void send(float f, hls::stream<transmit_t> &fifo) {
+#pragma HLS INLINE
+
     transmit_t temp;
-    temp.f = f;
+    temp.i = (int32_t)(f * 65536); //IBR changed to int.
+    //temp.f = f;
     fifo.write(temp);
 }
 
 void send(data_t & point, hls::stream<transmit_t> & fifo) {
+#pragma HLS INLINE
+
     uint32_t i;
 
     for (i = 0; i < DIMENSIONS; i++) {
@@ -66,13 +70,10 @@ void recv(uint32_t &ui, hls::stream<transmit_t> &fifo) {
 void recv(int32_t &i, hls::stream<transmit_t> &fifo) {
 	i = fifo.read().i;
 }
-/*
-void recv(fixed_t &f, hls::stream<transmit_t> &fifo) {
-	f(31, 0) = fifo.read().ui;
-}
-*/
+
 void recv(float &f, hls::stream<transmit_t> &fifo) {
-    f = fifo.read().f;
+    f = (fifo.read().i * 1.0) / 65536;
+    //f = fifo.read().f;
 }
 
 void recv(data_t & point, hls::stream<transmit_t> & fifo) {

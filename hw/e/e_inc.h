35036b2b3c1b14c39b7b43b9549eee95b7fefb2f
ibrahimahmed <Ibrahim.ahmed73@gmail.com>
2015-07-13 15:49:48 -0400

    Update e_inc.h

--- hw/e/e_inc.h
+++ hw/e/e_inc.h
@@ -3,11 +3,17 @@
 
     #include "../common/common.h"
     #include <stdint.h>
-    #include <hls_stream.h>
 
-    void e(hls::stream<float> & e_bram_in, hls::stream<float> & e_bram_out,
-           hls::stream<float> & e_fifo, hls::stream<float> & k1,
-           hls::stream<float> & k2, float y1_delta_alpha1_product,
-           float y2_delta_alpha2_product, float delta_b);
+    #ifdef FULL_INTEG
+        #include <hls_stream.h>
+        void e(float e_bram[ELEMENTS],
+               hls::stream<float> & e_fifo, hls::stream<float> & k1,
+               hls::stream<float> & k2, float y1_delta_alpha1_product,
+               float y2_delta_alpha2_product, float delta_b);
+
+    #else
+        void e(float e_bram[ELEMENTS], float e_fifo[ELEMENTS], float k1[ELEMENTS], float k2[ELEMENTS],
+                float y1_delta_alpha1_product, float y2_delta_alpha2_product, float delta_b);
+    #endif
 
 #endif

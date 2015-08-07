f1ef582a506cdd6f3275ae676aa930677e2a0412
Justin Tai <taijusti@users.noreply.github.com>
2015-08-07 15:34:15 -0400

    clean up main.cpp which is run on ZYNQ

--- sw/test_sw/main.cc
+++ sw/test_sw/main.cc
@@ -1,4 +1,7 @@
+// Distributed SMO SVM
+// Ibrahim Ahmed, Justin Tai, Patrick Wu
 // ECE1373 Digital Systems Design for SoC
+// University of Toronto
 
 #include <stdbool.h>
 #include <stdlib.h>
@@ -61,10 +64,10 @@ typedef union {
 	u32 ui;
 } convert_t;
 
-XDevice device_inst;
+XDevice device_inst_0;
+XDevice device_inst_1;
 XHost host_inst;
 XLlFifo host_debug_fifo;
-XLlFifo device_debug_fifo;
 
 static float randFloat(void) {
     return (float)rand() / RAND_MAX;
@@ -231,7 +234,6 @@ bool examineExample(int d2Idx, data_t training_data [ELEMENTS], bool y [ELEMENTS
     float maxErr = -1;
     float err, err1, err2;
     err2 = classify(alpha, b, training_data, y, training_data[d2Idx]) - (y[d2Idx] ? 1 : -1);
-    //static uint32_t start = 0;
 
     // do something with this point only if it is a kKT violator
     if (sw_kkt(alpha[d2Idx], y[d2Idx], err2)) {
@@ -263,7 +265,6 @@ bool examineExample(int d2Idx, data_t training_data [ELEMENTS], bool y [ELEMENTS
     // at a random point, looking for a valid point to
     // take a step on
     int start = rand() % ELEMENTS;
-    //start++;
     for (i = start % ELEMENTS; i < start + ELEMENTS; i++)
     {
         if ((alpha[i % ELEMENTS] != 0) && (alpha[i % ELEMENTS] != C)) {
@@ -280,7 +281,6 @@ bool examineExample(int d2Idx, data_t training_data [ELEMENTS], bool y [ELEMENTS
     // non-C examples, we now iterate over all examples as
     // a final effort to take a step on this point
     start = rand() % ELEMENTS;
-    //start++;
     for (i = start % ELEMENTS; i < start + ELEMENTS; i++) {
         err1 = classify(alpha, b, training_data, y, training_data[i % ELEMENTS])
                 - (y[i % ELEMENTS] ? 1 : -1);
@@ -303,45 +303,16 @@ void sw_host(data_t training_data [ELEMENTS], bool y [ELEMENTS],
     b = 0;
     iterations = 0;
     bool changed = false;
-    //bool examineAll = false;
+
     do {
         changed = false;
 
-        //if (examineAll) {
-            for ( j = 0; j < ELEMENTS; j++) {
-                changed |= examineExample(j, training_data, y, alpha, b);
-            }
-        //}
-        //else {
-        //    for (j = 0; j < ELEMENTS; j++) {
-        //        if ((alpha[j] != 0) && (alpha[j] != C)) {
-        //            changed |= examineExample(j, training_data, y, alpha, b);
-        //        }
-        //    }
-        //}
-
-        //if (examineAll)
-        //    examineAll = false;
-
-        //else if (!changed) // continue looping through the non-bound example until no change occurs
-        //    examineAll = true;
+		for ( j = 0; j < ELEMENTS; j++) {
+			changed |= examineExample(j, training_data, y, alpha, b);
+		}
 
         iterations++;
     } while(changed);
-}
-
-static int TxSend(XLlFifo *InstancePtr, u32 word) {
-	//xil_printf(" Transmitting Data ... \r\n");
-
-	while(!XLlFifo_iTxVacancy(InstancePtr));
-
-	XLlFifo_TxPutWord(InstancePtr, word);
-
-	XLlFifo_iTxSetLen(InstancePtr, 1);
-
-	while( !(XLlFifo_IsTxDone(InstancePtr)));
-
-	return XST_SUCCESS;
 }
 
 int RxReceive (XLlFifo *InstancePtr, u32* word) {
@@ -356,330 +327,10 @@ int RxReceive (XLlFifo *InstancePtr, u32* word) {
 
 	*word = XLlFifo_RxGetWord(InstancePtr);
 
-	//status = XLlFifo_IsRxDone(InstancePtr);
-	//if(Status != TRUE){
-	//	xil_printf("Failing in receive complete ... \r\n");
-	//	return XST_FAILURE;
-	//}
-
 	return XST_SUCCESS;
 }
 
-/*
-static bool test_device(void) {
-    data_t data[ELEMENTS];
-    data_t point1;
-    data_t point2;
-    bool y[ELEMENTS];
-    float y1_delta_alpha1_product;
-    float y2_delta_alpha2_product;
-    float delta_b;
-    float e_bram[ELEMENTS];
-    float expected_e_bram[ELEMENTS];
-    float max_delta_e;
-    float expected_max_delta_e;
-    uint32_t max_delta_e_idx;
-    uint32_t expected_max_delta_e_idx;
-    float target_e;
-    float alpha[ELEMENTS];
-    float k1[ELEMENTS];
-    float k2[ELEMENTS];
-    uint32_t kkt_bram[ELEMENTS];
-    uint32_t expected_kkt_bram[ELEMENTS];
-    uint32_t kkt_violators;
-    uint32_t expected_kkt_violators;
-    uint32_t i;
-    uint32_t j;
-	uint32_t status;
-	uint32_t temp;
-	float temp_f;
-	bool temp_b;
-
-    ////////////////////////////////////////////////////////////
-    /////////GENERATE INPUT VECTOR / EXPECTED OUTPUT////////////
-    ////////////////////////////////////////////////////////////
-
-    // initialize everything
-    y1_delta_alpha1_product = randFloat();
-    y2_delta_alpha2_product = randFloat();
-    delta_b = randFloat();
-    for (i = 0; i < ELEMENTS; i++) {
-        y[i] = randFloat() > 0.5;
-        e_bram[i] = y[i] ? -1 : 1;
-        alpha[i] = randFloat();
-
-        for (j = 0; j < DIMENSIONS; j++) {
-            data[i].dim[j] = randFloat();
-        }
-    }
-
-    for (i = 0; i < DIMENSIONS; i++) {
-        point1.dim[i] = randFloat();
-        point2.dim[i] = randFloat();
-    }
-
-    target_e = randFloat();
-
-    // calculate correct answers
-    for (i = 0; i < ELEMENTS; i++) {
-        k1[i] = sw_k(point1, data[i]);
-        k2[i] = sw_k(point2, data[i]);
-    }
-
-    for (i = 0; i < ELEMENTS; i++) {
-        expected_e_bram[i] = sw_e(e_bram[i], k1[i], k2[i],
-                                  y1_delta_alpha1_product,
-                                  y2_delta_alpha2_product,
-                                  delta_b);
-    }
-
-    j = 0;
-    for (i = 0; i < ELEMENTS; i++) {
-        if (!sw_kkt(alpha[i], y[i], expected_e_bram[i])) {
-            expected_kkt_bram[j] = i;
-            j++;
-        }
-    }
-    expected_kkt_violators = j;
-
-    expected_max_delta_e = 0;
-    expected_max_delta_e_idx = 0;
-    for (i = 0; i < ELEMENTS; i++) {
-    	float delta_e = ABS(expected_e_bram[i] - target_e);
-
-        if (delta_e > expected_max_delta_e) {
-            expected_max_delta_e = delta_e;
-            expected_max_delta_e_idx = i;
-        }
-    }
-
-    ////////////////////////////////////////////////////////////
-    //////////////CONFIGURE THE DEVICE//////////////////////////
-    ////////////////////////////////////////////////////////////
-
-    // initialize device
-    TxSend(&device_fifo, COMMAND_INIT_DATA);
-    for (i = 0; i < ELEMENTS; i++) {
-        for (j = 0; j < DIMENSIONS; j++) {
-        	TxSend(&device_fifo, (uint32_t)(data[i].dim[j] * 65536));
-        }
-
-        TxSend(&device_fifo, y[i]);
-    }
-
-    // set the points
-    TxSend(&device_fifo, COMMAND_SET_POINT_0);
-    for (i = 0; i < DIMENSIONS; i++) {
-    	TxSend(&device_fifo, (uint32_t)(point1.dim[i] * 65536));
-    }
-
-    TxSend(&device_fifo, COMMAND_SET_POINT_1);
-    for (i = 0; i < DIMENSIONS; i++) {
-    	TxSend(&device_fifo, (uint32_t)(point2.dim[i] * 65536));
-    }
-
-    //set the alphas
-    for (i = 0; i < ELEMENTS; i++) {
-        TxSend(&device_fifo, COMMAND_SET_ALPHA);
-        TxSend(&device_fifo, i);
-        TxSend(&device_fifo, (uint32_t)(alpha[i] * 65536));
-    }
-
-    // set the delta alpha products
-    TxSend(&device_fifo, COMMAND_SET_Y1_ALPHA1_PRODUCT);
-    TxSend(&device_fifo, (uint32_t)(y1_delta_alpha1_product * 65536));
-
-    TxSend(&device_fifo, COMMAND_SET_Y2_ALPHA2_PRODUCT);
-    TxSend(&device_fifo, (uint32_t)(y2_delta_alpha2_product * 65536));
-
-    // set delta B
-    TxSend(&device_fifo, COMMAND_SET_DELTA_B);
-    TxSend(&device_fifo, (uint32_t)(delta_b * 65536));
-
-    // set target E
-    TxSend(&device_fifo, COMMAND_SET_E);
-    TxSend(&device_fifo, (uint32_t)(target_e * 65536));
-
-    ////////////////////////////////////////////////////////////
-    //////////////CHECK ALL CONFIGURATIONS//////////////////////
-    ////////////////////////////////////////////////////////////
-
-	// check E's
-    for (i = 0; i < ELEMENTS; i++) {
-        TxSend(&device_fifo, COMMAND_GET_E);
-        TxSend(&device_fifo, i);
-        RxReceive(&device_fifo, &temp);
-        temp_f = y[i] ? -1 : 1;
-        temp_f = (float)((int32_t)temp) / 65536.0;
-
-        if (temp_f != (y[i] ? -1 : 1)) {
-        	return false;
-        }
-    }
-
-    // check that the points were properly set
-    for (i = 0; i < ELEMENTS; i++) {
-     	TxSend(&device_fifo, COMMAND_GET_POINT);
-     	TxSend(&device_fifo, i);
-
-     	for (j = 0; j < DIMENSIONS; j++) {
-     		RxReceive(&device_fifo, &temp);
-
-     		if (ABS((float)temp / 65536.0 - data[i].dim[j]) > ERROR ) {
-     			return false;
-     		}
-     	}
-    }
-
-    // check that the chosen points were properly set
-    TxSend(&device_fifo, COMMAND_GET_POINT_0);
-    for (i = 0; i < DIMENSIONS; i++) {
-        do {
-        	status = XLlFifo_iRxOccupancy(&device_fifo);
-        } while(status == 0);
-     	RxReceive(&device_fifo, &temp);
-
-     	if (ABS((float)temp / 65536.0 - point1.dim[i]) > ERROR) {
-     		return false;
-     	}
-    }
-
-    TxSend(&device_fifo, COMMAND_GET_POINT_1);
-    for (i = 0; i < DIMENSIONS; i++) {
-        do {
-        	status = XLlFifo_iRxOccupancy(&device_fifo);
-        } while(status == 0);
-     	RxReceive(&device_fifo, &temp);
-
-     	if (ABS((float)temp / 65536.0 - point2.dim[i]) > ERROR) {
-     		return 1;
-    	}
-    }
-
-    // check that the alphas were properly set
-    for (i = 0; i < ELEMENTS; i++) {
-    	TxSend(&device_fifo, COMMAND_GET_ALPHA);
-    	TxSend(&device_fifo, i);
-        do {
-        	status = XLlFifo_iRxOccupancy(&device_fifo);
-        } while(status == 0);
-    	RxReceive(&device_fifo, &temp);
-
-    	if (ABS((float)temp / 65536.0 - alpha[i]) > ERROR) {
-    		return false;
-    	}
-    }
-
-    // check y delta alpha products
-    TxSend(&device_fifo, COMMAND_GET_Y1_ALPHA1_PRODUCT);
-	RxReceive(&device_fifo, &temp);
-
-	if (ABS((float)temp / 65536.0 - y1_delta_alpha1_product) > ERROR) {
-		return false;
-	}
-
-	TxSend(&device_fifo, COMMAND_GET_Y2_ALPHA2_PRODUCT);
-	RxReceive(&device_fifo, &temp);
-
-	if (ABS((float)temp / 65536.0 - y2_delta_alpha2_product) > ERROR) {
-		return false;
-	}
-
-	// check delta b
-	TxSend(&device_fifo, COMMAND_GET_DELTA_B);
-	RxReceive(&device_fifo, &temp);
-
-	if (ABS((float)temp / 65536.0 - delta_b) > ERROR) {
-		return false;
-	}
-
-	// check target e
-	TxSend(&device_fifo, COMMAND_GET_TARGET_E);
-	RxReceive(&device_fifo, &temp);
-
-	if (ABS((float)temp / 65536.0 - target_e) > ERROR) {
-		return false;
-	}
-
-    ////////////////////////////////////////////////////////////
-    ////////////////COMPUTE EVERYTHING//////////////////////////
-    ////////////////////////////////////////////////////////////
-
-    // compute and get KKT violators
-    TxSend(&device_fifo, COMMAND_GET_KKT);
-    do {
-    	status = XLlFifo_iRxOccupancy(&device_fifo);
-    } while(status == 0);
-    RxReceive(&device_fifo, &kkt_violators);
-    for (i = 0; i < kkt_violators; i++) {
-    	RxReceive(&device_fifo, &(kkt_bram[i]));
-    }
-
-    // compute delta E
-    TxSend(&device_fifo, COMMAND_GET_DELTA_E);
-    do {
-       	status = XLlFifo_iRxOccupancy(&device_fifo);
-    } while(status == 0);
-    RxReceive(&device_fifo, &temp);
-    max_delta_e = (float)temp / 65536.0;
-    RxReceive(&device_fifo, &max_delta_e_idx);
-
-    ////////////////////////////////////////////////////////////
-    //////////////////GET ALL RESULTS///////////////////////////
-    ////////////////////////////////////////////////////////////
-
-    // ask for all E values
-    for (i = 0; i < ELEMENTS; i++) {
-        TxSend(&device_fifo, COMMAND_GET_E);
-        TxSend(&device_fifo, i);
-        RxReceive(&device_fifo, &temp);
-        e_bram[i] = (float)temp / 65536.0;
-    }
-
-    ////////////////////////////////////////////////////////////
-    //////////////////CHECKING PHASE////////////////////////////
-    ////////////////////////////////////////////////////////////
-
-    // check if the # of kkt violators match our expected # of kkt violators
-    if (expected_kkt_violators != kkt_violators) {
-        printf("TEST FAILED! # of KKT violators mismatch! expected: %ld\tactual: %ld\n",
-               expected_kkt_violators, kkt_violators);
-        return false;
-    }
-
-    // check if the kkt violating entries match our expected kkt violators
-    for (i = 0; i < kkt_violators; i++) {
-        if (kkt_bram[i] != expected_kkt_bram[i]) {
-            printf("TEST FAILED! KKT violator entry mismatch!\n");
-            return false;
-        }
-    }
-
-    // check if all the Es match up
-    for (i = 0; i < ELEMENTS; i++) {
-        if (ABS(expected_e_bram[i] - e_bram[i]) > TEST_ERROR) {
-            printf("TEST FAILED! E mismatch!\n");
-            return false;
-        }
-    }
-
-    // check that the max delta e value matches with our expected max delta e
-    if (ABS(max_delta_e - expected_max_delta_e) > TEST_ERROR) {
-        printf("TEST FAILED! MAX_DELTA_E mismatch!\n");
-        return false;
-    }
-
-    if (max_delta_e_idx != expected_max_delta_e_idx) {
-    	return false;
-    }
-
-    printf("TEST PASSED!\n");
-    return true;
-}
-*/
-
 static bool test_host(void) {
-
 	data_t data[ELEMENTS];
 	bool y[ELEMENTS];
 	float expected_alpha [ELEMENTS];
@@ -749,45 +400,12 @@ static bool test_host(void) {
 
 	// forward fifo communication to device
 	uint32_t occupancy;
-	uint32_t kkt_violators;
-	uint32_t point1_idx;
-	uint32_t point2_idx;
-	uint32_t state = 0;
-
 	while (XHost_IsDone(&host_inst) == 0) {
 		occupancy = XLlFifo_iRxOccupancy(&host_debug_fifo);
 		for (i = 0; i < occupancy; i++) {
 			assert(host_inst.Axi_bus_BaseAddress == XPAR_XHOST_0_S_AXI_AXI_BUS_BASEADDR);
 			RxReceive(&host_debug_fifo, &temp);
-
-			switch (state) {
-			default:
-			case 0:
-				state = 1;
-				assert(temp == 0xdeadbeef);
-				break;
-
-			case 1:
-				state = 2;
-				kkt_violators = temp;
-				break;
-
-			case 2:
-				state = 3;
-				point1_idx = temp;
-				break;
-
-			case 3:
-				state = 0;
-				point2_idx = temp;
-				break;
-			}
-		}
-
-		occupancy = XLlFifo_iRxOccupancy(&device_debug_fifo);
-		for (i = 0; i < occupancy; i++) {
-			assert(host_inst.Axi_bus_BaseAddress == XPAR_XHOST_0_S_AXI_AXI_BUS_BASEADDR);
-			RxReceive(&device_debug_fifo, &temp);
+			assert(temp != 0xdeadbeef);
 		}
 	}
 
@@ -815,24 +433,17 @@ int main(void) {
 	XLlFifo_Config * host_debug_fifo_cfg = XLlFfio_LookupConfig(XPAR_HOST_DEBUG_FIFO_DEVICE_ID);
 
 	// initialize device and host
-	XDevice_Initialize(&device_inst, 0);
-	XHost_Initialize(&host_inst, 0);
+	XDevice_Initialize(&device_inst_0, XPAR_DEVICE_0_DEVICE_ID);
+	XDevice_Initialize(&device_inst_1, XPAR_DEVICE_1_DEVICE_ID);
+	XHost_Initialize(&host_inst, XPAR_HOST_0_DEVICE_ID);
 
     // configure host/device fifos
-	//XLlFifo_CfgInitialize(&device_debug_fifo, device_debug_fifo_cfg, device_debug_fifo_cfg->BaseAddress);
-    //XLlFifo_IntClear(&device_debug_fifo, 0xffffffff);
 	XLlFifo_CfgInitialize(&host_debug_fifo, host_debug_fifo_cfg, host_debug_fifo_cfg->BaseAddress);
     XLlFifo_IntClear(&host_debug_fifo, 0xffffffff);
 
 	// start the device
-	XDevice_Start(&device_inst);
-
-	/*
-	if (test_device() == false) {
-		while(1);
-		return 1;
-	}
-	*/
+	XDevice_Start(&device_inst_0);
+	XDevice_Start(&device_inst_1);
 
 	if (test_host() == false) {
 		while(1);
